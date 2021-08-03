88bdfea85d3c8828613e9f5e2178e2ee81ef80d6
Nathan <nathan.flacher.pro@gmail.com>
2021-08-03 16:49:39 +0200

    in progress

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { CarsListComponent } from './cars-list/cars-list.component';
+import { DriversListComponent } from './drivers-list/drivers-list.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { FourOhFourComponent } from './four-oh-four/four-oh-four.component';
+import { TemplateComponent } from './template/template.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CarsListComponent,
+    DriversListComponent,
+    FooterComponent,
+    HomePageComponent,
+    FourOhFourComponent,
+    TemplateComponent
   ],
   imports: [
     BrowserModule,
